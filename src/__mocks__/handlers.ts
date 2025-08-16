d0e62a51c881c145b6dc8963492a7a149cd37cfe
tksemf7410 <tksemf7410@gmail.com>
2025-08-16 22:26:37 +0900

    feat: API 호출 테스트를 위한 MSW 준비

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,47 @@
 import { http, HttpResponse } from 'msw';
+import { randomUUID } from 'crypto';
 
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
 import { Event } from '../types';
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
+
+const serverState = { events };
+
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json(serverState.events);
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    const events = serverState.events;
+    const newEvent = { id: randomUUID(), ...request.body } as Event;
+    serverState.events = [...events, newEvent];
 
-  http.put('/api/events/:id', async ({ params, request }) => {}),
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const events = serverState.events;
+    const { id } = params;
+    const eventIndex = events.findIndex((e) => e.id === id);
+    if (eventIndex > -1) {
+      const newEvents = [...events];
+      newEvents[eventIndex] = { ...events[eventIndex], ...request.body };
+
+      serverState.events = newEvents;
+      return HttpResponse.json(events[eventIndex]);
+    } else {
+      return new HttpResponse('Event not found', { status: 404 });
+    }
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const events = serverState.events;
+    const { id } = params;
+
+    serverState.events = events.filter((e) => e.id !== id);
+    return new HttpResponse(null, { status: 204 });
+  }),
 ];
