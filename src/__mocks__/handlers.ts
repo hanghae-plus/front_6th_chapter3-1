1db710d2c8b7ccae5abeaaaa56268ffcabbe2262
jsheo <tjrwjdgj@gmail.com>
2025-08-18 01:22:56 +0900

    feat: handlers 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -6,11 +6,34 @@ import { Event } from '../types';
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    console.log('handlers events', events);
+    return HttpResponse.json(events);
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post('/api/events', async ({ request }) => {
+    console.log('handlers post', request);
+    const newEvent = await request.json() as Event;
+    newEvent.id = `${events.length + 1}`;
+    return HttpResponse.json(newEvent);
+  }),
 
-  http.put('/api/events/:id', async ({ params, request }) => {}),
+  http.put('/api/events/:id', async ({ params, request }) => {
+    console.log('handlers put', params, request);
+    const reqs = await request.json() as Event[];
+    const selctedIndex = reqs.findIndex((item) => item.id === params.id);
+    if (selctedIndex > -1) {
+      const newEvents = [...reqs];
+      newEvents[selctedIndex] = { ...newEvents[selctedIndex], ...reqs[selctedIndex] };
+      return HttpResponse.json(newEvents[selctedIndex]);
+    }
+    return HttpResponse.json(null, { status: 404 });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.delete('/api/events/:id', ({ params }) => {
+    console.log('handlers delete', params);
+    const { id } = params;
+    events.filter((item) => item.id !== id);
+    return new Response(null, { status: 204 });
+  }),
 ];
