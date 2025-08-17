d8b6c0043caa353b49391551f5be6222c92efa19
bebusl <bebus1998@naver.com>
2025-08-18 00:06:49 +0900

    feat: mockHandlers 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,46 @@
 import { http, HttpResponse } from 'msw';
-
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
-import { Event } from '../types';
+import { EventForm } from '../types';
+
+import { randomUUID } from 'crypto';
+
+let mockEvents = [...events];
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json(mockEvents);
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post<{}, EventForm>('/api/events', async ({ request }) => {
+    const body = await request.json();
+    const newEvent = { id: randomUUID(), ...body };
 
-  http.put('/api/events/:id', async ({ params, request }) => {}),
+    mockEvents.push(newEvent);
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
+
+  http.put<{ id: string }, EventForm>('/api/events/:id', async ({ params, request }) => {
+    const { id } = params;
+    const body = await request.json();
+
+    const eventIndex = mockEvents.findIndex((event) => event.id === id);
+
+    if (eventIndex > -1) {
+      mockEvents[eventIndex] = { ...mockEvents[eventIndex], ...body };
+      return HttpResponse.json(mockEvents[eventIndex]);
+    } else {
+      return HttpResponse.text('Event not found', { status: 404 });
+    }
+  }),
+
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+
+    mockEvents.filter((event) => event.id !== id);
+
+    return new HttpResponse(null, { status: 204 });
+  }),
 ];
