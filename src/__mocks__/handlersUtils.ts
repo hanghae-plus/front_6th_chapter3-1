e4380bf279f743226cc2f103d6c79b32c5b3cd3d
ldhldh07 <dhlim07@naver.com>
2025-08-18 23:39:01 +0900

    feat: handlersUtils 정의

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -1,10 +1,72 @@
+import { http, HttpResponse } from 'msw';
 import { Event } from '../types';
+import { randomUUID } from 'crypto';
 
 // ! Hard
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
-export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {};
+export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
+  const store = { current: structuredClone(initEvents) as Event[] };
 
-export const setupMockHandlerUpdating = () => {};
+  return [
+    http.get('/api/events', () => {
+      return HttpResponse.json(
+        { events: store.current },
+        {
+          status: 200,
+        }
+      );
+    }),
+    http.post('/api/events', async ({ request }) => {
+      const requestJson = (await request.json()) as Omit<Event, 'id'>;
+      const newEvent = {
+        id: randomUUID(),
+        ...requestJson,
+      };
+      return HttpResponse.json(newEvent, { status: 201 });
+    }),
+  ];
+};
 
-export const setupMockHandlerDeletion = () => {};
+export const setupMockHandlerUpdating = (initEvents: Event[] = []) => {
+  const store = { current: structuredClone(initEvents) as Event[] };
+  return [
+    http.get('/api/events', () => {
+      return HttpResponse.json(
+        { events: store.current },
+        {
+          status: 200,
+        }
+      );
+    }),
+    http.put('/api/events/:id', async ({ params: { id }, request }) => {
+      const eventList = store.current as Event[];
+      const updatedIndex = eventList.findIndex((event) => event.id === id);
+      if (updatedIndex < 0) return HttpResponse.json({ message: 'Not Found' }, { status: 404 });
+
+      const updatedEvent = (await request.json()) as Partial<Event>;
+      const resultEvent = { ...eventList[updatedIndex], ...updatedEvent, id };
+      return HttpResponse.json(resultEvent, { status: 200 });
+    }),
+  ];
+};
+
+export const setupMockHandlerDeletion = (initEvents: Event[] = []) => {
+  const store = { current: structuredClone(initEvents) as Event[] };
+
+  return [
+    http.get('/api/events', () => {
+      return HttpResponse.json(
+        { events: store.current },
+        {
+          status: 200,
+        }
+      );
+    }),
+    http.delete('/api/events/:id', ({ params: { id } }) => {
+      const hasEvent = (store.current as Event[]).some((event) => event.id === id);
+      if (!hasEvent) return HttpResponse.json({ message: 'Not Found' }, { status: 404 });
+      return new HttpResponse(null, { status: 204 });
+    }),
+  ];
+};
