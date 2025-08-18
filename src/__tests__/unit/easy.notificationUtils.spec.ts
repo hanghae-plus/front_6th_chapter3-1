52f18916ce69b5115127382bd61e4ee77986f26a
BangDori <qudwns8865@naver.com>
2025-08-19 00:23:25 +0900

    test: 알림 이벤트

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -1,16 +1,45 @@
 import { Event } from '../../types';
 import { createNotificationMessage, getUpcomingEvents } from '../../utils/notificationUtils';
 
+const event = {
+  id: '1',
+  date: '2025-10-01',
+  startTime: '10:00:00',
+  endTime: '11:00:00',
+  title: 'test',
+  notificationTime: 10,
+};
+
 describe('getUpcomingEvents', () => {
-  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {});
+  it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
+    const result = getUpcomingEvents([{ ...event }], new Date('2025-10-01T09:50:00'), []);
 
-  it('이미 알림이 간 이벤트는 제외한다', () => {});
+    expect(result).toEqual([event]);
+  });
 
-  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {});
+  it('이미 알림이 간 이벤트는 제외한다', () => {
+    const result = getUpcomingEvents([{ ...event }], new Date('2025-10-01T10:00:00'), ['1']);
 
-  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {});
+    expect(result).toEqual([]);
+  });
+
+  it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
+    const result = getUpcomingEvents([{ ...event }], new Date('2025-10-01T09:40:00'), []);
+
+    expect(result).toEqual([]);
+  });
+
+  it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
+    const result = getUpcomingEvents([{ ...event }], new Date('2025-10-01T10:00:00'), []);
+
+    expect(result).toEqual([]);
+  });
 });
 
 describe('createNotificationMessage', () => {
-  it('올바른 알림 메시지를 생성해야 한다', () => {});
+  it('올바른 알림 메시지를 생성해야 한다', () => {
+    const result = createNotificationMessage({ notificationTime: 10, title: 'test' });
+
+    expect(result).toBe('10분 후 test 일정이 시작됩니다.');
+  });
 });
