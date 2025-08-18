8034ba25029c986c05fb69b87a6da989ade4b50d
Yuyeol <uy23@naver.com>
2025-08-18 10:29:54 +0900

    test: parseDateTime - 단위테스트 작성

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -5,14 +5,32 @@ import {
   isOverlapping,
   parseDateTime,
 } from '../../utils/eventOverlap';
+
 describe('parseDateTime', () => {
-  it('2025-07-01 14:30을 정확한 Date 객체로 변환한다', () => {});
+  it('2025-07-01 14:30을 동일한 날짜와 시간을 가진 Date 객체로 변환한다', () => {
+    const expectedDateTime = new Date('2025-07-01T14:30');
+    const parsedDateTime = parseDateTime('2025-07-01', '14:30');
+    expect(parsedDateTime).toBeInstanceOf(Date);
+    expect(parsedDateTime.getTime()).toBe(expectedDateTime.getTime());
+  });
 
-  it('잘못된 날짜 형식에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 날짜 형식에 대해 Invalid Date를 반환한다', () => {
+    const parsedDateTime = parseDateTime('2025년7월1일', '14:30');
+    expect(parsedDateTime).toBeInstanceOf(Date);
+    expect(parsedDateTime.toString()).toBe('Invalid Date');
+  });
 
-  it('잘못된 시간 형식에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 시간 형식에 대해 Invalid Date를 반환한다', () => {
+    const parsedDateTime = parseDateTime('2025-07-01', '14시30분');
+    expect(parsedDateTime).toBeInstanceOf(Date);
+    expect(parsedDateTime.toString()).toBe('Invalid Date');
+  });
 
-  it('날짜 문자열이 비어있을 때 Invalid Date를 반환한다', () => {});
+  it('날짜 문자열이 비어있을 때 Invalid Date를 반환한다', () => {
+    const parsedDateTime = parseDateTime('', '14:30');
+    expect(parsedDateTime).toBeInstanceOf(Date);
+    expect(parsedDateTime.toString()).toBe('Invalid Date');
+  });
 });
 
 describe('convertEventToDateRange', () => {
