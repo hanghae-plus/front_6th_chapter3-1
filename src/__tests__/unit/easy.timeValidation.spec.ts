d3b5f1d1cfd0ed6978640d2750e5e51bf7f3b92c
areumH <dkfmal0109@gmail.com>
2025-08-19 02:53:41 +0900

    feat: timeValidation 테스트 코드 작성

--- src/__tests__/unit/easy.timeValidation.spec.ts
+++ src/__tests__/unit/easy.timeValidation.spec.ts
@@ -1,15 +1,39 @@
 import { getTimeErrorMessage } from '../../utils/timeValidation';
 
 describe('getTimeErrorMessage >', () => {
-  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 늦을 때 에러 메시지를 반환한다', () => {
+    const errorMessage = getTimeErrorMessage('15:00', '10:00');
+    expect(errorMessage.startTimeError).toBe('시작 시간은 종료 시간보다 빨라야 합니다.');
+    expect(errorMessage.endTimeError).toBe('종료 시간은 시작 시간보다 늦어야 합니다.');
+  });
 
-  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {});
+  it('시작 시간과 종료 시간이 같을 때 에러 메시지를 반환한다', () => {
+    const errorMessage = getTimeErrorMessage('10:00', '10:00');
+    expect(errorMessage.startTimeError).toBe('시작 시간은 종료 시간보다 빨라야 합니다.');
+    expect(errorMessage.endTimeError).toBe('종료 시간은 시작 시간보다 늦어야 합니다.');
+  });
 
-  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {});
+  it('시작 시간이 종료 시간보다 빠를 때 null을 반환한다', () => {
+    const errorMessage = getTimeErrorMessage('10:00', '15:00');
+    expect(errorMessage.startTimeError).toBe(null);
+    expect(errorMessage.endTimeError).toBe(null);
+  });
 
-  it('시작 시간이 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간이 비어있을 때 null을 반환한다', () => {
+    const errorMessage = getTimeErrorMessage('', '15:00');
+    expect(errorMessage.startTimeError).toBe(null);
+    expect(errorMessage.endTimeError).toBe(null);
+  });
 
-  it('종료 시간이 비어있을 때 null을 반환한다', () => {});
+  it('종료 시간이 비어있을 때 null을 반환한다', () => {
+    const errorMessage = getTimeErrorMessage('10:00', '');
+    expect(errorMessage.startTimeError).toBe(null);
+    expect(errorMessage.endTimeError).toBe(null);
+  });
 
-  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {});
+  it('시작 시간과 종료 시간이 모두 비어있을 때 null을 반환한다', () => {
+    const errorMessage = getTimeErrorMessage('', '');
+    expect(errorMessage.startTimeError).toBe(null);
+    expect(errorMessage.endTimeError).toBe(null);
+  });
 });
