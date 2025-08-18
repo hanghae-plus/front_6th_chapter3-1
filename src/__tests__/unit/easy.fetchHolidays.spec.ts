7c0fb7abb6281492bf35d7b56252e08745872d74
HYOJIN <hyo.khj@gmail.com>
2025-08-18 23:50:00 +0900

    test: fetchHolidays 테스트 추가

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,8 +1,28 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date('2025-08-18');
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    expect(fetchHolidays(date)).toEqual({
+      '2025-08-15': '광복절',
+    });
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date('2025-07-18');
+
+    expect(fetchHolidays(date)).toEqual({});
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date('2025-10-18');
+
+    expect(fetchHolidays(date)).toEqual({
+      '2025-10-03': '개천절',
+      '2025-10-09': '한글날',
+      '2025-10-05': '추석',
+      '2025-10-06': '추석',
+      '2025-10-07': '추석',
+    });
+  });
 });
