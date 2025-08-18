8e52ea0381030bda8544f9ded02c620a5dbfc521
ure <uy23@naver.com>
2025-08-19 00:26:46 +0900

    test: useNotifications - 지정 시간 알림 동작 단위테스트 작성

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -5,8 +5,7 @@ import { Event } from '../../types.ts';
 import { formatDate } from '../../utils/dateUtils.ts';
 import { parseHM } from '../utils.ts';
 
-// 기존 '초기 상태'는 모호함. msw는 기본적으로 이벤트가 존재하므로,
-// Events 빈배열인 상태를 초기 상태로 가정
+// msw는 기본적으로 이벤트가 존재하므로, Events 빈배열인 상태를 초기 상태로 가정
 it('초기 상태에서는 알림이 없어야 한다', () => {
   const events: Event[] = [];
   const { result } = renderHook(() => useNotifications(events));
@@ -15,7 +14,45 @@ it('초기 상태에서는 알림이 없어야 한다', () => {
   expect(result.current.notifiedEvents).toEqual([]);
 });
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', async () => {
+  // 가상의 시간을 현재 시간으로 설정
+  const mockNow = new Date('2025-01-01T09:50:00');
+  vi.useFakeTimers();
+  vi.setSystemTime(mockNow);
+
+  const events: Event[] = [
+    {
+      id: '1',
+      title: '테스트 미팅',
+      date: formatDate(mockNow),
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '테스트',
+      location: '테스트 장소',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    },
+  ];
+
+  const { result } = renderHook(() => useNotifications(events));
+
+  expect(result.current.notifications).toHaveLength(0);
+
+  // notification을 트리거하기 위해 미래로 1초 보냄
+  // 10분 이내 어느 미래로 보내도 상관없어야하는데 1초마다 notification이 트리거되는 버그가 있음.
+  await act(async () => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+  expect(result.current.notifications[0]).toEqual({
+    id: '1',
+    message: '10분 후 테스트 미팅 일정이 시작됩니다.',
+  });
+
+  vi.useRealTimers();
+});
 
 it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
 
