bcddd611eb5511427f7f87bb2535eec7020a04fa
sangsoo <wsckm1@gmail.com>
2025-08-19 09:54:15 +0900

    test: getWeeksAtMonth 함수 테스트 코드 작성

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -159,7 +159,19 @@ describe('getWeekDates', () => {
 });
 
 describe('getWeeksAtMonth', () => {
-  it('2025년 7월 1일의 올바른 주 정보를 반환해야 한다', () => {});
+  it('2025년 7월 1일의 올바른 주 정보를 반환해야 한다', () => {
+    const weeksAtMonth = getWeeksAtMonth(new Date('2025-07-01'));
+
+    const expected = [
+      [null, null, 1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24, 25, 26],
+      [27, 28, 29, 30, 31, null, null],
+    ];
+
+    expect(weeksAtMonth).to.deep.equal(expected);
+  });
 });
 
 describe('getEventsForDay', () => {
