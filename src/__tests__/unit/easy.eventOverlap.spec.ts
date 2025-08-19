00c5cf6a49de8776bd657243aa2b0918771bfa2d
jihyeon <rachel2148072@gmail.com>
2025-08-20 00:57:36 +0900

    test: findOverlappingEvents 작성

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -138,7 +138,92 @@ describe('isOverlapping', () => {
 });
 
 describe('findOverlappingEvents', () => {
-  it('새 이벤트와 겹치는 모든 이벤트를 반환한다', () => {});
+  const events: Event[] = [
+    {
+      id: '2b7545a6-ebee-426c-b906-2329bc8d62bd',
+      title: '팀 회의',
+      date: '2025-08-20',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '주간 팀 미팅',
+      location: '회의실 A',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    },
+    {
+      id: '09702fb3-a478-40b3-905e-9ab3c8849dcd',
+      title: '점심 약속',
+      date: '2025-08-21',
+      startTime: '12:30',
+      endTime: '13:30',
+      description: '동료와 점심 식사',
+      location: '회사 근처 식당',
+      category: '개인',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    },
+    {
+      id: 'da3ca408-836a-4d98-b67a-ca389d07552b',
+      title: '프로젝트 마감',
+      date: '2025-08-25',
+      startTime: '09:00',
+      endTime: '18:00',
+      description: '분기별 프로젝트 마감',
+      location: '사무실',
+      category: '업무',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 1,
+    },
+  ];
+  it('새 이벤트와 겹치는 모든 이벤트를 반환한다', () => {
+    expect(
+      findOverlappingEvents(
+        {
+          title: '프로젝트 마감',
+          date: '2025-08-25',
+          startTime: '10:00',
+          endTime: '17:00',
+          description: '분기별 프로젝트 마감',
+          location: '사무실',
+          category: '업무',
+          repeat: { type: 'none', interval: 0 },
+          notificationTime: 1,
+        },
+        events
+      )
+    ).toEqual([
+      {
+        id: 'da3ca408-836a-4d98-b67a-ca389d07552b',
+        title: '프로젝트 마감',
+        date: '2025-08-25',
+        startTime: '09:00',
+        endTime: '18:00',
+        description: '분기별 프로젝트 마감',
+        location: '사무실',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 1,
+      },
+    ]);
+  });
 
-  it('겹치는 이벤트가 없으면 빈 배열을 반환한다', () => {});
+  it('겹치는 이벤트가 없으면 빈 배열을 반환한다', () => {
+    expect(
+      findOverlappingEvents(
+        {
+          title: '프로젝트 마감',
+          date: '2025-08-25',
+          startTime: '18:00',
+          endTime: '19:00',
+          description: '분기별 프로젝트 마감',
+          location: '사무실',
+          category: '업무',
+          repeat: { type: 'none', interval: 0 },
+          notificationTime: 1,
+        },
+        events
+      )
+    ).toEqual([]);
+  });
 });
