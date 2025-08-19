ddab59db4eb7cb49ca582e69f11074754cb06c1e
JiHoon-0330 <ljihoon0330@gmail.com>
2025-08-20 00:16:45 +0900

    getWeeksAtMonth 테스트 추가

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -272,7 +272,15 @@ describe('getWeekDates 주어진 날짜가 속한 주의 모든 날짜를 반환
 });
 
 describe('getWeeksAtMonth', () => {
-  it('2025년 7월 1일의 올바른 주 정보를 반환해야 한다', () => {});
+  it('해당 월의 주 정보를 반환한다 (2025-07-01)', () => {
+    expect(getWeeksAtMonth(new Date('2025-07-01'))).toEqual([
+      [null, null, 1, 2, 3, 4, 5],
+      [6, 7, 8, 9, 10, 11, 12],
+      [13, 14, 15, 16, 17, 18, 19],
+      [20, 21, 22, 23, 24, 25, 26],
+      [27, 28, 29, 30, 31, null, null],
+    ]);
+  });
 });
 
 describe('getEventsForDay', () => {
