2f43de8c777ea70cb28bc3eaaf4aaab95c9c95f6
jihyeon <rachel2148072@gmail.com>
2025-08-19 23:45:54 +0900

    test: parseDateTime 작성

--- src/__tests__/unit/easy.eventOverlap.spec.ts
+++ src/__tests__/unit/easy.eventOverlap.spec.ts
@@ -5,14 +5,23 @@ import {
   isOverlapping,
   parseDateTime,
 } from '../../utils/eventOverlap';
+
 describe('parseDateTime', () => {
-  it('2025-07-01 14:30을 정확한 Date 객체로 변환한다', () => {});
+  it('2025-07-01 14:30을 정확한 Date 객체로 변환한다', () => {
+    expect(parseDateTime('2025-07-01', '14:30')).toBeInstanceOf(Date);
+  });
 
-  it('잘못된 날짜 형식에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 날짜 형식에 대해 Invalid Date를 반환한다', () => {
+    expect(parseDateTime('20250701', '14:30').toString()).toBe('Invalid Date');
+  });
 
-  it('잘못된 시간 형식에 대해 Invalid Date를 반환한다', () => {});
+  it('잘못된 시간 형식에 대해 Invalid Date를 반환한다', () => {
+    expect(parseDateTime('2025-07-01', '1430').toString()).toBe('Invalid Date');
+  });
 
-  it('날짜 문자열이 비어있을 때 Invalid Date를 반환한다', () => {});
+  it('날짜 문자열이 비어있을 때 Invalid Date를 반환한다', () => {
+    expect(parseDateTime('20250701', '').toString()).toBe('Invalid Date');
+  });
 });
 
 describe('convertEventToDateRange', () => {
