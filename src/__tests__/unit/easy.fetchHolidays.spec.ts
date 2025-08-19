89125e51f82e91c785c47c2ad6d402694386ee0a
YeongseoYoon <l112xpla@gmail.com>
2025-08-19 23:03:00 +0900

    test: fetchHolidays 함수의 공휴일 테스트 케이스 수정 및 불필요한 테스트 제거

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -15,7 +15,7 @@ describe('fetchHolidays', () => {
     });
   });
 
-  describe('단일 공휴일이 있는 월', () => {
+  describe('공휴일이 있는 월', () => {
     test('3월에는 삼일절만 반환한다', () => {
       const marchDate = new Date('2025-03-15');
       const result = fetchHolidays(marchDate);
@@ -34,35 +34,6 @@ describe('fetchHolidays', () => {
 
       expect(result).toEqual({});
       expect(Object.keys(result)).toHaveLength(0);
-    });
-  });
-
-  describe('여러 공휴일이 있는 월', () => {
-    test('1월에는 신정과 설날 3일을 모두 반환한다', () => {
-      const januaryDate = new Date('2025-01-15');
-      const result = fetchHolidays(januaryDate);
-
-      expect(result).toEqual({
-        '2025-01-01': '신정',
-        '2025-01-29': '설날',
-        '2025-01-30': '설날',
-        '2025-01-31': '설날',
-      });
-      expect(Object.keys(result)).toHaveLength(4);
-    });
-
-    test('10월에는 개천절, 추석 3일, 한글날을 모두 반환한다', () => {
-      const octoberDate = new Date('2025-10-15');
-      const result = fetchHolidays(octoberDate);
-
-      expect(result).toEqual({
-        '2025-10-03': '개천절',
-        '2025-10-05': '추석',
-        '2025-10-06': '추석',
-        '2025-10-07': '추석',
-        '2025-10-09': '한글날',
-      });
-      expect(Object.keys(result)).toHaveLength(5);
     });
   });
 });
