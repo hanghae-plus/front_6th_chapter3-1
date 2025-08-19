291c40180e4d8f5580e24285faa826ba7944a703
ldhldh07 <dhlim07@naver.com>
2025-08-19 14:39:09 +0900

    test: 핸들러 유틸에 에러처리 받는 인자 추가

--- src/__mocks__/handlersUtils.ts
+++ src/__mocks__/handlersUtils.ts
@@ -4,20 +4,30 @@ import { http, HttpResponse } from 'msw';
 
 import { Event } from '../types';
 
+type RequestMethod = 'get' | 'post' | 'put' | 'delete';
+
+type IsSuccess<M extends RequestMethod> = Record<`${M}IsSuccess`, boolean>;
+
 // ! Hard
 // ! 이벤트는 생성, 수정 되면 fetch를 다시 해 상태를 업데이트 합니다. 이를 위한 제어가 필요할 것 같은데요. 어떻게 작성해야 테스트가 병렬로 돌아도 안정적이게 동작할까요?
 // ! 아래 이름을 사용하지 않아도 되니, 독립적이게 테스트를 구동할 수 있는 방법을 찾아보세요. 그리고 이 로직을 PR에 설명해주세요.
-export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
+export const setupMockHandlerCreation = (
+  initEvents = [] as Event[],
+  successFlags: Partial<IsSuccess<'get' | 'post'>> = {}
+) => {
   const store = { current: structuredClone(initEvents) as Event[] };
+  const { getIsSuccess = true, postIsSuccess = true } = successFlags;
 
   return [
     http.get('/api/events', () => {
-      return HttpResponse.json(
-        { events: store.current },
-        {
-          status: 200,
-        }
-      );
+      if (getIsSuccess)
+        return HttpResponse.json(
+          { events: store.current },
+          {
+            status: 200,
+          }
+        );
+      return HttpResponse.json({ message: 'fetch fail' }, { status: 500 });
     }),
     http.post('/api/events', async ({ request }) => {
       const requestJson = (await request.json()) as Omit<Event, 'id'>;
@@ -25,21 +35,29 @@ export const setupMockHandlerCreation = (initEvents = [] as Event[]) => {
         id: randomUUID(),
         ...requestJson,
       };
-      return HttpResponse.json(newEvent, { status: 201 });
+      if (postIsSuccess) return HttpResponse.json(newEvent, { status: 201 });
+      return HttpResponse.json({ message: 'not found' }, { status: 404 });
     }),
   ];
 };
 
-export const setupMockHandlerUpdating = (initEvents: Event[] = []) => {
+export const setupMockHandlerUpdating = (
+  initEvents: Event[] = [],
+  successFlags: Partial<IsSuccess<'get' | 'put'>> = {}
+) => {
+  const { getIsSuccess = true } = successFlags;
+
   const store = { current: structuredClone(initEvents) as Event[] };
   return [
     http.get('/api/events', () => {
-      return HttpResponse.json(
-        { events: store.current },
-        {
-          status: 200,
-        }
-      );
+      if (getIsSuccess)
+        return HttpResponse.json(
+          { events: store.current },
+          {
+            status: 200,
+          }
+        );
+      return HttpResponse.json({ message: 'fetch fail' }, { status: 500 });
     }),
     http.put('/api/events/:id', async ({ params, request }) => {
       const id = params.id as string;
