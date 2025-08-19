5cdb65fa9817eb9d3f869b0040844287895febf1
Ycow <skypnal12@gmail.com>
2025-08-19 22:49:52 +0900

    feat: Create시 utils 이용하도록 수정, 일정 수정 테스트 코드 추가

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,6 +1,6 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { render, screen, within, act, waitFor } from '@testing-library/react';
+import { render, screen, within, act, waitFor, getByRole } from '@testing-library/react';
 import { UserEvent, userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import { SnackbarProvider } from 'notistack';
@@ -9,6 +9,7 @@ import { ReactElement } from 'react';
 import App from '../App';
 import { server } from '../setupTests';
 import { Event, EventForm } from '../types';
+import { setupMockHandlerCreation, setupMockHandlerUpdating } from '../__mocks__/handlersUtils';
 
 const AppWrapper = () => {
   const theme = createTheme();
@@ -24,8 +25,22 @@ const AppWrapper = () => {
 };
 
 describe('일정 CRUD 및 기본 기능', () => {
-  it('새로운 일정을 추가하면 입력한 모든 정보가 이벤트 리스트에 표시된다.', async () => {
+  it('폼 데이터를 입력하고 일정 추가 버튼을 클릭하면 새로운 일정이 리스트에 추가된다.', async () => {
     // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+    setupMockHandlerCreation([
+      {
+        id: '1',
+        title: '기존 회의',
+        date: '2025-10-15',
+        startTime: '09:00',
+        endTime: '10:00',
+        description: '기존 팀 미팅',
+        location: '회의실 B',
+        category: '업무',
+        repeat: { type: 'none', interval: 0 },
+        notificationTime: 10,
+      },
+    ]);
 
     render(<AppWrapper />);
 
@@ -42,13 +57,32 @@ describe('일정 CRUD 및 기본 기능', () => {
 
     await user.click(within(selectCategory).getByRole('combobox'));
     await user.click(screen.getByRole('option', { name: `기타-option` }));
-
     await user.click(screen.getByRole('button', { name: '일정 추가' }));
 
+    expect(await screen.findAllByRole('button', { name: 'Edit event' })).toHaveLength(2);
     expect(screen.getByText('새로운 팀 미팅')).toBeInTheDocument();
   });
 
-  it('일정을 수정하면 수정된 정보가 이벤트 리스트에 반영된다.', async () => {});
+  it('일정을 수정하면 수정된 정보가 이벤트 리스트에 반영된다.', async () => {
+    setupMockHandlerUpdating();
+
+    render(<AppWrapper />);
+
+    const user = userEvent.setup();
+    const editButton = await screen.findByRole('button', { name: 'Edit event' });
+    const locationInput = screen.getByLabelText('위치');
+
+    await user.click(editButton);
+
+    expect(screen.getByRole('button', { name: '일정 수정' })).toBeInTheDocument();
+    expect(screen.getByDisplayValue('기존 회의')).toBeInTheDocument();
+
+    await user.clear(locationInput);
+    await user.type(locationInput, '회의실 C');
+    await user.click(screen.getByRole('button', { name: '일정 수정' }));
+
+    expect(screen.getByText('회의실 C')).toBeInTheDocument();
+  });
 
   it('일정을 삭제하고 더 이상 조회되지 않는지 확인한다', async () => {});
 });
