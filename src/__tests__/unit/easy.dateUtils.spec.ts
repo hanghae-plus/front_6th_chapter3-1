21b42fb70e9746de7fd918201e93795d3a536992
JiHoon-0330 <ljihoon0330@gmail.com>
2025-08-20 00:35:50 +0900

    isDateInRange 테스트 추가

--- src/__tests__/unit/easy.dateUtils.spec.ts
+++ src/__tests__/unit/easy.dateUtils.spec.ts
@@ -374,20 +374,34 @@ describe('formatMonth', () => {
 
 describe('isDateInRange', () => {
   it('범위 내의 날짜 2025-07-10에 대해 true를 반환한다', () => {
-    expect(
-      isDateInRange(new Date('2025-07-10'), new Date('2025-07-01'), new Date('2025-07-31'))
-    ).toBe(true);
+    const range: [Date, Date] = [new Date('2025-07-01'), new Date('2025-07-31')];
+    expect(isDateInRange(new Date('2025-07-10'), ...range)).toBe(true);
   });
 
-  it('범위의 시작일 2025-07-01에 대해 true를 반환한다', () => {});
+  it('범위의 시작일 2025-07-01에 대해 true를 반환한다', () => {
+    const range: [Date, Date] = [new Date('2025-07-01'), new Date('2025-07-31')];
+    expect(isDateInRange(new Date('2025-07-01'), ...range)).toBe(true);
+  });
 
-  it('범위의 종료일 2025-07-31에 대해 true를 반환한다', () => {});
+  it('범위의 종료일 2025-07-31에 대해 true를 반환한다', () => {
+    const range: [Date, Date] = [new Date('2025-07-01'), new Date('2025-07-31')];
+    expect(isDateInRange(new Date('2025-07-31'), ...range)).toBe(true);
+  });
 
-  it('범위 이전의 날짜 2025-06-30에 대해 false를 반환한다', () => {});
+  it('범위 이전의 날짜 2025-06-30에 대해 false를 반환한다', () => {
+    const range: [Date, Date] = [new Date('2025-07-01'), new Date('2025-07-31')];
+    expect(isDateInRange(new Date('2025-06-30'), ...range)).toBe(false);
+  });
 
-  it('범위 이후의 날짜 2025-08-01에 대해 false를 반환한다', () => {});
+  it('범위 이후의 날짜 2025-08-01에 대해 false를 반환한다', () => {
+    const range: [Date, Date] = [new Date('2025-07-01'), new Date('2025-07-31')];
+    expect(isDateInRange(new Date('2025-08-01'), ...range)).toBe(false);
+  });
 
-  it('시작일이 종료일보다 늦은 경우 모든 날짜에 대해 false를 반환한다', () => {});
+  it('시작일이 종료일보다 늦은 경우 모든 날짜에 대해 false를 반환한다', () => {
+    const range: [Date, Date] = [new Date('2025-07-31'), new Date('2025-07-01')];
+    expect(isDateInRange(new Date('2025-07-10'), ...range)).toBe(false);
+  });
 });
 
 describe('fillZero', () => {
