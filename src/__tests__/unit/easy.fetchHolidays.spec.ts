96215112dd9df49c23f670248b2c47775d512c12
이태영 <lhlhgodz3@gmail.com>
2025-08-19 23:07:07 +0900

    :white_check_mark: easy.fetchHolidays.spec.ts 및 easy.notificationUtils.spec.ts 테스트 코드 작성 완료!

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,8 +1,25 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date('2025-08-01');
+    const holidays = fetchHolidays(date);
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    expect(holidays).toHaveProperty('2025-08-15');
+    expect(holidays['2025-08-15']).toBe('광복절');
+  });
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date('2025-11-01');
+    const holidays = fetchHolidays(date);
+
+    expect(holidays).toEqual({});
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date('2025-10-01');
+    const holidays = fetchHolidays(date);
+
+    expect(holidays).toHaveProperty('2025-10-03');
+    expect(holidays).toHaveProperty('2025-10-09');
+  });
 });
