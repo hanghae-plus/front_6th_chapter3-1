929d1386c49142124d8dd7bddf5b43e8af4b6ce6
jun17183 <jun17183@naver.com>
2025-08-19 15:11:46 +0900

    test. fetchHolidays 작성

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -1,8 +1,40 @@
 import { fetchHolidays } from '../../apis/fetchHolidays';
+
 describe('fetchHolidays', () => {
-  it('주어진 월의 공휴일만 반환한다', () => {});
+  it('주어진 월의 공휴일만 반환한다', () => {
+    const date = new Date('2025-08-01');
+    const expected = {
+      '2025-08-15': '광복절',
+    };
 
-  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {});
+    const result = fetchHolidays(date);
 
-  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {});
+    expect(result).toEqual(expected);
+    expect(Object.keys(result).length).toBe(1);
+  });
+
+  it('공휴일이 없는 월에 대해 빈 객체를 반환한다', () => {
+    const date = new Date('2025-07-01');
+    const expected = {};
+
+    const result = fetchHolidays(date);
+
+    expect(result).toEqual(expected);
+    expect(Object.keys(result).length).toBe(0);
+  });
+
+  it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const date = new Date('2025-01-01');
+    const expected = {
+      '2025-01-01': '신정',
+      '2025-01-29': '설날',
+      '2025-01-30': '설날',
+      '2025-01-31': '설날',
+    };
+
+    const result = fetchHolidays(date);
+
+    expect(result).toEqual(expected);
+    expect(Object.keys(result).length).toBe(4);
+  });
 });
