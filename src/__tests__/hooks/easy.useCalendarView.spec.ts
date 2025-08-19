7bf0af04ceba816053e5416922ee0f95420cc936
areumH <dkfmal0109@gmail.com>
2025-08-19 18:54:27 +0900

    feat: useCalendarView 테스트 코드 작성

--- src/__tests__/hooks/easy.useCalendarView.spec.ts
+++ src/__tests__/hooks/easy.useCalendarView.spec.ts
@@ -1,24 +1,116 @@
+import { vi } from 'vitest';
 import { act, renderHook } from '@testing-library/react';
-
 import { useCalendarView } from '../../hooks/useCalendarView.ts';
 import { assertDate } from '../utils.ts';
 
 describe('초기 상태', () => {
-  it('view는 "month"이어야 한다', () => {});
+  beforeEach(() => {
+    vi.useFakeTimers();
+    vi.setSystemTime(new Date('2025-10-01T00:00:00'));
+  });
 
-  it('currentDate는 오늘 날짜인 "2025-10-01"이어야 한다', () => {});
+  afterEach(() => {
+    vi.useRealTimers();
+  });
 
-  it('holidays는 10월 휴일인 개천절, 한글날, 추석이 지정되어 있어야 한다', () => {});
+  it('view는 "month"이어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    expect(result.current.view).toBe('month');
+  });
+
+  it('currentDate는 오늘 날짜인 "2025-10-01"이어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    const date = new Date('2025-10-01');
+    expect(result.current.currentDate).toEqual(date);
+  });
+
+  it('holidays는 10월 휴일인 개천절, 한글날, 추석이 지정되어 있어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    const expected = {
+      '2025-10-05': '추석',
+      '2025-10-06': '추석',
+      '2025-10-07': '추석',
+      '2025-10-03': '개천절',
+      '2025-10-09': '한글날',
+    };
+
+    expect(result.current.holidays).toEqual(expected);
+  });
 });
 
-it("view를 'week'으로 변경 시 적절하게 반영된다", () => {});
+it("view를 'week'으로 변경 시 적절하게 반영된다", () => {
+  const { result } = renderHook(() => useCalendarView());
+  expect(result.current.view).toBe('month');
 
-it("주간 뷰에서 다음으로 navigate시 7일 후 '2025-10-08' 날짜로 지정이 된다", () => {});
+  act(() => {
+    result.current.setView('week');
+  });
 
-it("주간 뷰에서 이전으로 navigate시 7일 후 '2025-09-24' 날짜로 지정이 된다", () => {});
+  expect(result.current.view).toBe('week');
+});
 
-it("월간 뷰에서 다음으로 navigate시 한 달 후 '2025-11-01' 날짜여야 한다", () => {});
+it("주간 뷰에서 다음으로 navigate시 7일 후 '2025-10-08' 날짜로 지정이 된다", () => {
+  const { result } = renderHook(() => useCalendarView());
+  const date = new Date('2025-10-08');
 
-it("월간 뷰에서 이전으로 navigate시 한 달 전 '2025-09-01' 날짜여야 한다", () => {});
+  act(() => {
+    result.current.setView('week');
+  });
 
-it("currentDate가 '2025-03-01' 변경되면 3월 휴일 '삼일절'로 업데이트되어야 한다", async () => {});
+  act(() => {
+    result.current.navigate('next');
+  });
+
+  expect(result.current.currentDate).toEqual(date);
+});
+
+it("주간 뷰에서 이전으로 navigate시 7일 후 '2025-09-24' 날짜로 지정이 된다", () => {
+  const { result } = renderHook(() => useCalendarView());
+  const date = new Date('2025-09-24');
+
+  act(() => {
+    result.current.setView('week');
+  });
+
+  act(() => {
+    result.current.navigate('prev');
+  });
+
+  expect(result.current.currentDate).toEqual(date);
+});
+
+it("월간 뷰에서 다음으로 navigate시 한 달 후 '2025-11-01' 날짜여야 한다", () => {
+  const { result } = renderHook(() => useCalendarView());
+  const date = new Date('2025-11-01');
+
+  act(() => {
+    result.current.navigate('next');
+  });
+
+  expect(result.current.currentDate).toEqual(date);
+});
+
+it("월간 뷰에서 이전으로 navigate시 한 달 전 '2025-09-01' 날짜여야 한다", () => {
+  const { result } = renderHook(() => useCalendarView());
+  const date = new Date('2025-09-01');
+
+  act(() => {
+    result.current.navigate('prev');
+  });
+
+  expect(result.current.currentDate).toEqual(date);
+});
+
+it("currentDate가 '2025-03-01' 변경되면 3월 휴일 '삼일절'로 업데이트되어야 한다", async () => {
+  const { result } = renderHook(() => useCalendarView());
+  const date = new Date('2025-03-01');
+  const expected = {
+    '2025-03-01': '삼일절',
+  };
+
+  act(() => {
+    result.current.setCurrentDate(date);
+  });
+
+  expect(result.current.holidays).toEqual(expected);
+});
