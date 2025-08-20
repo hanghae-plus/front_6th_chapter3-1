98b3b6a9f91a5154800962fa1f517c168bf3a55f
tomatopickles404 <tomatopickles404@gmail.com>
2025-08-20 23:26:55 +0900

    test: fetchHolidays 테스트 코드 추가

--- src/__tests__/utils.ts
+++ src/__tests__/utils.ts
@@ -1,4 +1,6 @@
 import { fillZero } from '../utils/dateUtils';
+import { Event } from '../types';
+import events from '../__mocks__/response/events.json';
 
 export const assertDate = (date1: Date, date2: Date) => {
   expect(date1.toISOString()).toBe(date2.toISOString());
@@ -9,4 +11,33 @@ export const parseHM = (timestamp: number) => {
   const h = fillZero(date.getHours());
   const m = fillZero(date.getMinutes());
   return `${h}:${m}`;
+};
+
+export const createTestEvent = (overrides: Partial<Event> = {}) => {
+  const {
+    id,
+    title,
+    date,
+    startTime,
+    endTime,
+    description,
+    location,
+    category,
+    repeat,
+    notificationTime,
+  } = events.events[0];
+
+  return {
+    id,
+    title,
+    date,
+    startTime,
+    endTime,
+    description,
+    location,
+    category,
+    repeat,
+    notificationTime,
+    ...overrides,
+  } as Event;
 };
