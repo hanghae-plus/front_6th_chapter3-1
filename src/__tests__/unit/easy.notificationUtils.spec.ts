6c9d630aaad7086b6a50a1482cba055d407cc8ed
ldhldh07 <dhlim07@naver.com>
2025-08-20 09:23:13 +0900

    chore: 변수명 구체화

--- src/__tests__/unit/easy.notificationUtils.spec.ts
+++ src/__tests__/unit/easy.notificationUtils.spec.ts
@@ -3,18 +3,18 @@ import { createNotificationMessage, getUpcomingEvents } from '../../utils/notifi
 
 describe('getUpcomingEvents', () => {
   it('알림 시간이 정확히 도래한 이벤트를 반환한다', () => {
-    const dummyEvent = {
+    const notificationEvent = {
       id: '1',
       date: '2025-08-19',
       startTime: '16:00',
       notificationTime: 10,
     } as Event;
 
-    const eventStartTime = new Date(`${dummyEvent.date}T${dummyEvent.startTime}`);
-    const now = new Date(eventStartTime.getTime() - dummyEvent.notificationTime * 60000);
+    const eventStartTime = new Date(`${notificationEvent.date}T${notificationEvent.startTime}`);
+    const now = new Date(eventStartTime.getTime() - notificationEvent.notificationTime * 60000);
 
-    const result = getUpcomingEvents([dummyEvent], now, []);
-    const eventIndex = result.findIndex((event) => event.id === dummyEvent.id);
+    const result = getUpcomingEvents([notificationEvent], now, []);
+    const eventIndex = result.findIndex((event) => event.id === notificationEvent.id);
 
     expect(result[eventIndex]).toEqual({
       id: '1',
@@ -25,51 +25,55 @@ describe('getUpcomingEvents', () => {
   });
 
   it('이미 알림이 간 이벤트는 제외한다', () => {
-    const dummyEvent = {
+    const alreadyNotificatedEvent = {
       id: '1',
       date: '2025-08-19',
       startTime: '16:00',
       notificationTime: 10,
     } as Event;
 
-    const eventStartTime = new Date(`${dummyEvent.date}T${dummyEvent.startTime}`);
-    const now = new Date(eventStartTime.getTime() - dummyEvent.notificationTime * 60000);
+    const eventStartTime = new Date(
+      `${alreadyNotificatedEvent.date}T${alreadyNotificatedEvent.startTime}`
+    );
+    const now = new Date(
+      eventStartTime.getTime() - alreadyNotificatedEvent.notificationTime * 60000
+    );
 
-    const result = getUpcomingEvents([dummyEvent], now, [dummyEvent.id]);
+    const result = getUpcomingEvents([alreadyNotificatedEvent], now, [alreadyNotificatedEvent.id]);
 
     expect(result.length).toEqual(0);
   });
 
   it('알림 시간이 아직 도래하지 않은 이벤트는 반환하지 않는다', () => {
-    const dummyEvent = {
+    const notCommingEvent = {
       id: '1',
       date: '2025-08-19',
       startTime: '16:00',
       notificationTime: 10,
     } as Event;
 
-    const eventStartTime = new Date(`${dummyEvent.date}T${dummyEvent.startTime}`);
+    const eventStartTime = new Date(`${notCommingEvent.date}T${notCommingEvent.startTime}`);
     const beforeEventStartTime = eventStartTime.getTime() - 60000 * 10;
-    const now = new Date(beforeEventStartTime - dummyEvent.notificationTime * 6000);
+    const now = new Date(beforeEventStartTime - notCommingEvent.notificationTime * 60000);
 
-    const result = getUpcomingEvents([dummyEvent], now, []);
+    const result = getUpcomingEvents([notCommingEvent], now, []);
 
     expect(result.length).toEqual(0);
   });
 
   it('알림 시간이 지난 이벤트는 반환하지 않는다', () => {
-    const dummyEvent = {
+    const passedEvent = {
       id: '1',
       date: '2025-08-19',
       startTime: '16:00',
       notificationTime: 10,
     } as Event;
 
-    const eventStartTime = new Date(`${dummyEvent.date}T${dummyEvent.startTime}`);
+    const eventStartTime = new Date(`${passedEvent.date}T${passedEvent.startTime}`);
     const afterEventStartItme = eventStartTime.getTime() + 60000 * 10;
-    const now = new Date(afterEventStartItme - dummyEvent.notificationTime * 60000);
+    const now = new Date(afterEventStartItme - passedEvent.notificationTime * 60000);
 
-    const result = getUpcomingEvents([dummyEvent], now, []);
+    const result = getUpcomingEvents([passedEvent], now, []);
 
     expect(result.length).toEqual(0);
   });
@@ -77,14 +81,12 @@ describe('getUpcomingEvents', () => {
 
 describe('createNotificationMessage', () => {
   it('올바른 알림 메시지를 생성해야 한다', () => {
-    const dummyEvent = {
+    const event = {
       title: '모각코',
       notificationTime: 10,
     } as Event;
 
-    const result = createNotificationMessage(dummyEvent);
-    expect(result).toBe(
-      `${dummyEvent.notificationTime}분 후 ${dummyEvent.title} 일정이 시작됩니다.`
-    );
+    const result = createNotificationMessage(event);
+    expect(result).toBe(`${event.notificationTime}분 후 ${event.title} 일정이 시작됩니다.`);
   });
 });
