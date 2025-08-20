eee4a5886a96d47e34518a66e167050f34868361
devchangjun <ckdwns9121@naver.com>
2025-08-20 14:07:20 +0900

    first commit

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -3,6 +3,7 @@ import { http, HttpResponse } from 'msw';
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
 import { Event } from '../types';
 
+// ! TODO:
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
