4fec9cc6728b3fd10b9ea7e7fa7ab3e0b1d5f616
YeongseoYoon <l112xpla@gmail.com>
2025-08-21 00:33:03 +0900

    test: 테스트 환경 설정에서 가짜 타이머 사용 및 시스템 시간 설정 추가

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -8,18 +8,22 @@ export const server = setupServer(...handlers);
 
 beforeAll(() => {
   server.listen();
+  vi.useFakeTimers();
 });
 
 beforeEach(() => {
   expect.hasAssertions();
+  vi.setSystemTime(new Date('2025-08-21'));
 });
 
 afterEach(() => {
   server.resetHandlers();
   vi.clearAllMocks();
+  vi.clearAllTimers();
 });
 
 afterAll(() => {
   vi.resetAllMocks();
   server.close();
+  vi.useRealTimers();
 });
