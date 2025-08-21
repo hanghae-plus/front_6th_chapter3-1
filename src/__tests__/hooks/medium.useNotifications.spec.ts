18f3bacf4c3b04ffde70853f5a6bebddc71b7d15
soyalattee <uyyu1734@gmail.com>
2025-08-22 04:19:29 +0900

    feat: useNotifications 테스트 코드 작성

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -1,14 +1,99 @@
 import { act, renderHook } from '@testing-library/react';
 
 import { useNotifications } from '../../hooks/useNotifications.ts';
-import { Event } from '../../types.ts';
-import { formatDate } from '../../utils/dateUtils.ts';
-import { parseHM } from '../utils.ts';
+import { setTimeEventBeforeSecond } from '../../utils/dateUtils.ts';
+import { MOCK_EVENTS, LUNCH_0822 } from '../mockEvents.ts';
 
-it('초기 상태에서는 알림이 없어야 한다', () => {});
+it('초기 상태에서는 알림이 없어야 한다', () => {
+  const { result } = renderHook(() => useNotifications(MOCK_EVENTS));
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+  expect(result.current.notifications).toEqual([]);
+  expect(result.current.notifiedEvents).toEqual([]);
+});
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {
+  vi.useFakeTimers();
+  vi.setSystemTime(
+    setTimeEventBeforeSecond(
+      new Date(LUNCH_0822.date),
+      LUNCH_0822.startTime,
+      LUNCH_0822.notificationTime
+    )
+  );
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  const { result } = renderHook(() => useNotifications([LUNCH_0822]));
+
+  expect(result.current.notifications).toHaveLength(0);
+  expect(result.current.notifiedEvents).toHaveLength(0);
+
+  // 시간 1초 앞당기기
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+  expect(result.current.notifiedEvents).toHaveLength(1);
+
+  vi.useRealTimers();
+});
+
+it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {
+  vi.useFakeTimers();
+  vi.setSystemTime(
+    setTimeEventBeforeSecond(
+      new Date(LUNCH_0822.date),
+      LUNCH_0822.startTime,
+      LUNCH_0822.notificationTime
+    )
+  );
+
+  const { result } = renderHook(() => useNotifications([LUNCH_0822]));
+
+  // 시간 1초 앞당기기
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+  expect(result.current.notifiedEvents).toHaveLength(1);
+
+  // 알림 제거
+  act(() => {
+    result.current.removeNotification(0);
+  });
+
+  expect(result.current.notifications).toHaveLength(0);
+  expect(result.current.notifiedEvents).toHaveLength(1);
+
+  vi.useRealTimers();
+});
+
+it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {
+  vi.useFakeTimers();
+  vi.setSystemTime(
+    setTimeEventBeforeSecond(
+      new Date(LUNCH_0822.date),
+      LUNCH_0822.startTime,
+      LUNCH_0822.notificationTime
+    )
+  );
+
+  const { result } = renderHook(() => useNotifications([LUNCH_0822]));
+
+  expect(result.current.notifications).toHaveLength(0);
+  expect(result.current.notifiedEvents).toHaveLength(0);
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+  expect(result.current.notifiedEvents).toHaveLength(1);
+
+  act(() => {
+    vi.advanceTimersByTime(1000);
+  });
+
+  expect(result.current.notifications).toHaveLength(1);
+  expect(result.current.notifiedEvents).toHaveLength(1);
+});
