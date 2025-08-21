0eae2e0a1a9dd9c1ad9d352a47034a217b5d739b
hmyo2853 <hmyo2853@gmail.com>
2025-08-21 18:34:26 +0900

    Fix : CI 에러 수정

--- src/__tests__/unit/easy.fetchHolidays.spec.ts
+++ src/__tests__/unit/easy.fetchHolidays.spec.ts
@@ -40,28 +40,28 @@ describe('fetchHolidays', () => {
   });
 
   // 첫 테스트로 대체 가능하여 삭제
-  // it('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
-  //   const targetDate = new Date('2025-10-15');
+  it.skip('여러 공휴일이 있는 월에 대해 모든 공휴일을 반환한다', () => {
+    const targetDate = new Date('2025-10-15');
 
-  //   const result = fetchHolidays(targetDate);
+    const result = fetchHolidays(targetDate);
 
-  //   expect(Object.keys(result)).toHaveLength(5);
+    expect(Object.keys(result)).toHaveLength(5);
 
-  //   expect(result).toEqual({
-  //     '2025-10-03': '개천절',
-  //     '2025-10-05': '추석',
-  //     '2025-10-06': '추석',
-  //     '2025-10-07': '추석',
-  //     '2025-10-09': '한글날',
-  //   });
+    expect(result).toEqual({
+      '2025-10-03': '개천절',
+      '2025-10-05': '추석',
+      '2025-10-06': '추석',
+      '2025-10-07': '추석',
+      '2025-10-09': '한글날',
+    });
 
-  //   expect(result['2025-10-03']).toBe('개천절');
-  //   expect(result['2025-10-05']).toBe('추석');
-  //   expect(result['2025-10-06']).toBe('추석');
-  //   expect(result['2025-10-07']).toBe('추석');
-  //   expect(result['2025-10-09']).toBe('한글날');
+    expect(result['2025-10-03']).toBe('개천절');
+    expect(result['2025-10-05']).toBe('추석');
+    expect(result['2025-10-06']).toBe('추석');
+    expect(result['2025-10-07']).toBe('추석');
+    expect(result['2025-10-09']).toBe('한글날');
 
-  //   expect(result).not.toHaveProperty('2025-08-15');
-  //   expect(result).not.toHaveProperty('2025-12-25');
-  // });
+    expect(result).not.toHaveProperty('2025-08-15');
+    expect(result).not.toHaveProperty('2025-12-25');
+  });
 });
