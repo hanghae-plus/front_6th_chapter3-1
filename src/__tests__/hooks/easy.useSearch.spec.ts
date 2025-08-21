98139db370a4da612c60c88682dfe065a4a33c53
soyalattee <uyyu1734@gmail.com>
2025-08-22 02:02:09 +0900

    test: useSearch 테스트 코드 작성

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -1,14 +1,140 @@
 import { act, renderHook } from '@testing-library/react';
-
 import { useSearch } from '../../hooks/useSearch.ts';
 import { Event } from '../../types.ts';
 
-it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {});
+const TEST_DATE = new Date('2025-08-21');
 
-it('검색어에 맞는 이벤트만 필터링해야 한다', () => {});
+const METTING_0823: Event = {
+  id: '1',
+  title: '주간 회의',
+  date: '2025-08-23',
+  startTime: '14:00',
+  endTime: '15:00',
+  description: '주간 회의~',
+  location: 'A103',
+  category: '업무',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 10,
+};
+const LUNCH_0822: Event = {
+  id: '2',
+  title: '점심 약속',
+  date: '2025-08-22',
+  startTime: '12:00',
+  endTime: '13:00',
+  description: '친구랑 점심 약속~',
+  location: '롯데리아',
+  category: '개인',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 10,
+};
+const MEETING_0829: Event = {
+  id: '3',
+  title: '월간 회의',
+  date: '2025-08-29',
+  startTime: '14:00',
+  endTime: '15:00',
+  description: '월간 마무리 회의~',
+  location: 'A104',
+  category: '업무',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 30,
+};
+const LUNCH_0828: Event = {
+  id: '4',
+  title: '점심 데이트',
+  date: '2025-08-28',
+  startTime: '12:00',
+  endTime: '13:00',
+  description: '친구랑 점심 데이트, 후딱 먹고 회의가야함',
+  location: '버거킹',
+  category: '개인',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 30,
+};
+const MEETING_0926: Event = {
+  id: '5',
+  title: '9월 월간 회의',
+  date: '2025-09-26',
+  startTime: '14:00',
+  endTime: '15:00',
+  description: '9월 월간 회의~',
+  location: 'A104',
+  category: '업무',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 30,
+};
+const LUNCH_0904: Event = {
+  id: '6',
+  title: '9월 점심 약속',
+  date: '2025-09-04',
+  startTime: '12:00',
+  endTime: '13:00',
+  description: '9월 점심 약속~',
+  location: '야외 카페',
+  category: '개인',
+  repeat: { type: 'none', interval: 0 },
+  notificationTime: 30,
+};
+const MOCK_EVENTS: Event[] = [
+  METTING_0823,
+  LUNCH_0822,
+  MEETING_0829,
+  LUNCH_0828,
+  MEETING_0926,
+  LUNCH_0904,
+];
 
-it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {});
+it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(MOCK_EVENTS, TEST_DATE, 'month'));
 
-it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {});
+  const currentEvents = MOCK_EVENTS.filter((event) => event.date.startsWith('2025-08')); //8월 일정만 필터링
 
-it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {});
+  expect(result.current.filteredEvents).toEqual(currentEvents);
+});
+
+it('검색어에 맞는 이벤트만 필터링해야 한다', () => {
+  const { result } = renderHook(() => useSearch(MOCK_EVENTS, TEST_DATE, 'month'));
+
+  act(() => {
+    result.current.setSearchTerm('점심');
+  });
+
+  expect(result.current.filteredEvents).toEqual([LUNCH_0822, LUNCH_0828]);
+});
+
+//Q. 위에 테스트랑 중복되는 테스트 같음
+it('검색어가 제목, 설명, 위치 중 하나라도 일치하면 해당 이벤트를 반환해야 한다', () => {
+  const { result } = renderHook(() => useSearch(MOCK_EVENTS, TEST_DATE, 'month'));
+
+  act(() => {
+    result.current.setSearchTerm('회의');
+  });
+
+  // LUNCH_0828는 설명에 회의 키워드가 있다.
+  expect(result.current.filteredEvents).toEqual([METTING_0823, MEETING_0829, LUNCH_0828]);
+});
+
+it('현재 뷰(주간/월간)에 해당하는 이벤트만 반환해야 한다', () => {
+  const SEP_DATE = new Date('2025-09-01');
+
+  const { result } = renderHook(() => useSearch(MOCK_EVENTS, SEP_DATE, 'month')); //9월 일정 조회
+
+  expect(result.current.filteredEvents).toEqual([MEETING_0926, LUNCH_0904]);
+});
+
+it("검색어를 '회의'에서 '점심'으로 변경하면 필터링된 결과가 즉시 업데이트되어야 한다", () => {
+  const { result } = renderHook(() => useSearch(MOCK_EVENTS, TEST_DATE, 'month'));
+
+  act(() => {
+    result.current.setSearchTerm('회의');
+  });
+
+  expect(result.current.filteredEvents).toEqual([METTING_0823, MEETING_0829, LUNCH_0828]);
+
+  act(() => {
+    result.current.setSearchTerm('점심');
+  });
+
+  expect(result.current.filteredEvents).toEqual([LUNCH_0822, LUNCH_0828]);
+});
