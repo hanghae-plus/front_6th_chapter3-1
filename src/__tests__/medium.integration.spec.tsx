619487e837a6529b93d4b38e9bb1d84965c56462
ldhldh07 <dhlim07@naver.com>
2025-08-22 01:59:51 +0900

    feat: 일정 입력 및 저장 테스트 작성

--- src/__tests__/medium.integration.spec.tsx
+++ src/__tests__/medium.integration.spec.tsx
@@ -1,18 +1,96 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { render, screen, within, act } from '@testing-library/react';
+import { render, screen, within, act, renderHook, waitFor } from '@testing-library/react';
 import { UserEvent, userEvent } from '@testing-library/user-event';
 import { http, HttpResponse } from 'msw';
 import { SnackbarProvider } from 'notistack';
 import { ReactElement } from 'react';
 
+import { setupMockHandlerCreation } from '../__mocks__/handlersUtils';
 import App from '../App';
+import { useEventOperations } from '../hooks/useEventOperations';
 import { server } from '../setupTests';
 import { Event } from '../types';
 
 describe('일정 CRUD 및 기본 기능', () => {
   it('입력한 새로운 일정 정보에 맞춰 모든 필드가 이벤트 리스트에 정확히 저장된다.', async () => {
     // ! HINT. event를 추가 제거하고 저장하는 로직을 잘 살펴보고, 만약 그대로 구현한다면 어떤 문제가 있을 지 고민해보세요.
+    server.use(...setupMockHandlerCreation([]));
+
+    const user = userEvent.setup();
+
+    render(
+      <ThemeProvider theme={createTheme()}>
+        <SnackbarProvider>
+          <App />
+        </SnackbarProvider>
+      </ThemeProvider>
+    );
+
+    const titleInput = await screen.findByRole('textbox', { name: '제목' });
+    await user.type(await screen.findByRole('textbox', { name: '제목' }), '발제 모임');
+
+    expect(titleInput).toHaveValue('발제 모임');
+
+    const dateInput = await screen.findByLabelText('날짜');
+    await user.type(dateInput, '2025-08-24');
+
+    expect(dateInput).toHaveValue('2025-08-24');
+
+    const startTimeInput = await screen.findByLabelText('시작 시간');
+    await user.type(startTimeInput, '13:00');
+
+    expect(startTimeInput).toHaveValue('13:00');
+
+    const endTimeInput = await screen.findByLabelText('종료 시간');
+    await user.type(endTimeInput, '18:00');
+
+    expect(endTimeInput).toHaveValue('18:00');
+
+    const descriptionInput = await screen.findByRole('textbox', { name: '설명' });
+    await user.type(descriptionInput, '발제 시청');
+
+    expect(descriptionInput).toHaveValue('발제 시청');
+
+    const location = await screen.findByRole('textbox', { name: '위치' });
+    await user.type(location, '아이콘 빌딩');
+
+    expect(location).toHaveValue('아이콘 빌딩');
+
+    const categoryContainer = await screen.findByLabelText('카테고리');
+    const categorySelect = await within(categoryContainer).findByRole('combobox');
+    await user.click(categorySelect);
+    const workingSelectItem = screen.getByRole('option', { name: '업무-option' });
+    await user.click(workingSelectItem);
+
+    expect(categorySelect).toHaveTextContent('업무');
+
+    const notifyLabel = await screen.findByText('알림 설정', { selector: 'label' });
+    const notifySelect = await within(notifyLabel.parentElement!).findByRole('combobox');
+    await user.click(notifySelect);
+    const tenMinuiteBefore = await screen.findByLabelText('10분 전');
+    await user.click(tenMinuiteBefore);
+
+    expect(notifySelect).toHaveTextContent('10분 전');
+
+    const submitButton = screen.getByTestId('event-submit-button');
+
+    await user.click(submitButton);
+
+    await screen.findByText('일정이 추가되었습니다.');
+
+    await waitFor(() =>
+      expect(screen.queryByText('검색 결과가 없습니다.')).not.toBeInTheDocument()
+    );
+    const eventList = screen.getByTestId('event-list');
+
+    expect(await within(eventList).findByText('발제 모임')).toBeInTheDocument();
+    expect(within(eventList).getByText('2025-08-24')).toBeInTheDocument();
+    expect(within(eventList).getByText('13:00 - 18:00')).toBeInTheDocument();
+    expect(within(eventList).getByText('발제 시청')).toBeInTheDocument();
+    expect(within(eventList).getByText('아이콘 빌딩')).toBeInTheDocument();
+    expect(within(eventList).getByText('카테고리: 업무')).toBeInTheDocument();
+    expect(within(eventList).getByText('알림: 10분 전')).toBeInTheDocument();
   });
 
   it('기존 일정의 세부 정보를 수정하고 변경사항이 정확히 반영된다', async () => {});
