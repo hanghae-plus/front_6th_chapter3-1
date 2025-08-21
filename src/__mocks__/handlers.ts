c7e5b5c1ffd1e09b97ce45c5ea9e34f589757331
heojungseok <heojungseok@heojungseokui-MacBookAir.local>
2025-08-22 05:25:41 +0900

    test: handler 관련 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,27 +1,25 @@
+import { randomUUID } from 'crypto';
+
 import { http, HttpResponse } from 'msw';
 
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
 import { Event, EventForm } from '../types';
-import { randomUUID } from 'crypto';
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
   http.get('/api/events', () => {
-    console.log('handlers events', events);
     return HttpResponse.json(events);
   }),
 
   http.post('/api/events', async ({ request }) => {
-    console.log('handlers post', request);
-    const form = await request.json() as EventForm;
-    const newEvent: Event = {id: randomUUID(), ...form};
+    const form = (await request.json()) as EventForm;
+    const newEvent: Event = { id: randomUUID(), ...form };
     return HttpResponse.json(newEvent);
   }),
 
   http.put('/api/events/:id', async ({ params, request }) => {
-    console.log('handlers put', params, request);
-    const updateData = await request.json() as Partial<Event>;
+    const updateData = (await request.json()) as Partial<Event>;
     const selctedIndex = events.findIndex((item) => item.id === params.id);
     if (selctedIndex > -1) {
       const newEvents = [...events];
@@ -32,7 +30,6 @@ export const handlers = [
   }),
 
   http.delete('/api/events/:id', ({ params }) => {
-    console.log('handlers delete', params);
     const { id } = params;
     events.filter((item) => item.id !== id);
     return new Response(null, { status: 204 });
