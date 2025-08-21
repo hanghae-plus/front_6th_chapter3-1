7adf5ede0c583cc5cdfb74d003667705ea744c50
hmyo2853 <hmyo2853@gmail.com>
2025-08-21 22:15:13 +0900

    Feature : medium.useEventOperations.spec.ts 테스트 코드 추가

--- src/__tests__/hooks/medium.useNotifications.spec.ts
+++ src/__tests__/hooks/medium.useNotifications.spec.ts
@@ -1,14 +1,107 @@
-import { act, renderHook } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
 import { useNotifications } from '../../hooks/useNotifications.ts';
 import { Event } from '../../types.ts';
 import { formatDate } from '../../utils/dateUtils.ts';
 import { parseHM } from '../utils.ts';
 
-it('초기 상태에서는 알림이 없어야 한다', () => {});
+const initEvent: Event[] = [
+  {
+    id: '1',
+    title: '회의 시간',
+    date: '2025-07-15',
+    startTime: '10:10',
+    endTime: '11:10',
+    description: '팀 회의',
+    location: '회의실 A',
+    category: '업무',
+    repeat: { type: 'none', interval: 1 },
+    notificationTime: 10,
+  },
+];
 
-it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {});
+describe('useNotifications 초기 상태', () => {
+  beforeEach(() => {
+    vi.useRealTimers(); // 먼저 리셋
+    vi.useFakeTimers(); // 새로 설정
+  });
 
-it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {});
+  afterEach(() => {
+    vi.useRealTimers(); // 정리
+  });
 
-it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', () => {});
+  it('초기 상태에서는 알림이 없어야 한다', () => {
+    vi.setSystemTime(new Date('2025-07-15T10:00:00Z'));
+    const { result } = renderHook(() => useNotifications(initEvent));
+
+    expect(result.current.notifications).toEqual([]);
+  });
+});
+
+describe('useNotifications', () => {
+  beforeEach(() => {
+    vi.useRealTimers(); // 먼저 리셋
+    vi.useFakeTimers(); // 새로 설정
+  });
+
+  afterEach(() => {
+    vi.useRealTimers(); // 정리
+  });
+
+  it('지정된 시간이 된 경우 알림이 새롭게 생성되어 추가된다', () => {
+    const { result } = renderHook(() => useNotifications(initEvent));
+
+    vi.setSystemTime(new Date('2025-07-15T10:00:00Z')); // 알림 시간 10분 전
+
+    waitFor(() => {
+      expect(result.current.notifications).toHaveLength(1);
+      expect(result.current.notifications).toEqual({
+        id: 1,
+        message: '회의 시간은 10분 후에 시작됩니다.',
+      });
+    });
+  });
+
+  it('index를 기준으로 알림을 적절하게 제거할 수 있다', () => {
+    const { result } = renderHook(() => useNotifications(initEvent));
+
+    vi.setSystemTime(new Date('2025-07-15T10:00:00Z')); // 알림 시간 10분 전
+
+    waitFor(() => {
+      expect(result.current.notifications).toHaveLength(1);
+    });
+
+    act(() => {
+      result.current.removeNotification(0);
+    });
+
+    expect(result.current.notifications).toHaveLength(0);
+  });
+
+  it('이미 알림이 발생한 이벤트에 대해서는 중복 알림이 발생하지 않아야 한다', async () => {
+    const { result } = renderHook(() => useNotifications(initEvent));
+
+    vi.setSystemTime(new Date('2025-07-15T10:00:00Z')); // 알림 시간 10분 전
+
+    await act(async () => {
+      vi.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.notifications).toHaveLength(1);
+
+    expect(result.current.notifications).toEqual([
+      { id: '1', message: '10분 후 회의 시간 일정이 시작됩니다.' },
+    ]);
+
+    // 시간이 지나서 또 발생했는지 확인
+    await act(async () => {
+      vi.advanceTimersByTime(3000);
+    });
+
+    expect(result.current.notifications).toEqual([
+      { id: '1', message: '10분 후 회의 시간 일정이 시작됩니다.' },
+    ]);
+    expect(result.current.notifications).toHaveLength(1);
+    expect(result.current.notifiedEvents).toEqual(['1']);
+  });
+});
