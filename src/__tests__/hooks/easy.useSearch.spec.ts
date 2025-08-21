6cd5848cf8ae5ac28fff4357e3e5edf031a656d8
YeongseoYoon <l112xpla@gmail.com>
2025-08-22 01:36:26 +0900

    test: useSearch 훅에 대한 단위 테스트에서 목 데이터 수정

--- src/__tests__/hooks/easy.useSearch.spec.ts
+++ src/__tests__/hooks/easy.useSearch.spec.ts
@@ -1,48 +1,11 @@
 import { act, renderHook } from '@testing-library/react';
 
+import { events } from '../../__mocks__/fixture/mockEvents.json';
 import { useSearch } from '../../hooks/useSearch.ts';
 import { CalendarViewType, Event } from '../../types.ts';
 
-const mockEvents: Event[] = [
-  {
-    id: '1',
-    title: '면접공부',
-    date: '2025-08-17',
-    startTime: '10:00',
-    endTime: '11:00',
-    description: '면접 준비',
-    location: '내 방',
-    category: '개인',
-    repeat: { type: 'none', interval: 0 },
-    notificationTime: 10,
-  },
-  {
-    id: '2',
-    title: '회의',
-    date: '2025-08-17',
-    startTime: '12:00',
-    endTime: '13:00',
-    description: '회의',
-    location: '카페',
-    category: '업무',
-    repeat: { type: 'none', interval: 0 },
-    notificationTime: 10,
-  },
-  {
-    id: '3',
-    title: '코테풀기',
-    date: '2025-08-24',
-    startTime: '18:00',
-    endTime: '19:00',
-    description: '코테 풀기',
-    location: '릿코드',
-    category: '개인',
-    repeat: { type: 'none', interval: 0 },
-    notificationTime: 10,
-  },
-];
-
 const mockDate = new Date('2025-08-21');
+const mockEvents = events as Event[];
 
 it('검색어가 비어있을 때 모든 이벤트를 반환해야 한다', () => {
   const { result } = renderHook(() => useSearch(mockEvents, mockDate, CalendarViewType.MONTH));
