ceba3d825b426bb90a06a4e8bb228b0436bd5baf
jihyeon <rachel2148072@gmail.com>
2025-08-21 20:46:59 +0900

    test: msw handler 작성

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,37 @@
 import { http, HttpResponse } from 'msw';
+import { randomUUID } from 'crypto';
 
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
-import { Event } from '../types';
+import { Event, EventForm } from '../types';
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
+  }),
 
-  http.post('/api/events', async ({ request }) => {}),
+  http.post<never, EventForm, Event>('/api/events', async ({ request }) => {
+    const newEvent = await request.json();
+    const eventWithId: Event = {
+      ...newEvent,
+      id: randomUUID(),
+    };
 
-  http.put('/api/events/:id', async ({ params, request }) => {}),
+    return HttpResponse.json(eventWithId, { status: 201 });
+  }),
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+  http.put<{ id: string }, Event, Event>('/api/events/:id', async ({ params, request }) => {
+    const { id } = params;
+    const response = await request.json();
+
+    const updatedEvent = { ...response, id };
+
+    return HttpResponse.json(updatedEvent);
+  }),
+
+  http.delete<{ id: string }>('/api/events/:id', ({ params }) => {
+    const { id: _id } = params;
+    return new HttpResponse(null, { status: 204 });
+  }),
 ];
