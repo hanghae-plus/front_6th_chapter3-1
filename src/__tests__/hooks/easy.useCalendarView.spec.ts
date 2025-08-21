90d57c97dc3b3a3cbb8e92494feb499b0d3268ba
ldhldh07 <dhlim07@naver.com>
2025-08-21 17:07:55 +0900

    test: useCalendarView의 초기 상태 테스트 작성

--- src/__tests__/hooks/easy.useCalendarView.spec.ts
+++ src/__tests__/hooks/easy.useCalendarView.spec.ts
@@ -4,11 +4,34 @@ import { useCalendarView } from '../../hooks/useCalendarView.ts';
 import { assertDate } from '../utils.ts';
 
 describe('초기 상태', () => {
-  it('view는 "month"이어야 한다', () => {});
+  it('view는 "month"이어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    expect(result.current.view).toBe('month');
+  });
 
-  it('currentDate는 오늘 날짜인 "2025-10-01"이어야 한다', () => {});
+  it('currentDate는 입력된 당시의 날짜이어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    const todayDate = new Date(); // 2025-10-01
 
-  it('holidays는 10월 휴일인 개천절, 한글날, 추석이 지정되어 있어야 한다', () => {});
+    expect(result.current.currentDate.getFullYear()).toBe(todayDate.getFullYear());
+    expect(result.current.currentDate.getMonth()).toBe(todayDate.getMonth());
+    expect(result.current.currentDate.getDate()).toBe(todayDate.getDate());
+  });
+
+  it('holidays는 10월 휴일인 개천절, 한글날, 추석이 지정되어 있어야 한다', () => {
+    const { result } = renderHook(() => useCalendarView());
+    act(() => {
+      result.current.setCurrentDate(new Date('2025-10-01'));
+    });
+
+    const openSkyDay = '2025-10-03'; // 개천절
+    const koreanDay = '2025-10-06'; // 한글날
+    const chusuk = '2025-10-09'; // 추석
+
+    expect(result.current.holidays).toHaveProperty(openSkyDay);
+    expect(result.current.holidays).toHaveProperty(koreanDay);
+    expect(result.current.holidays).toHaveProperty(chusuk);
+  });
 });
 
 it("view를 'week'으로 변경 시 적절하게 반영된다", () => {});
