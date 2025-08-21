99fe5de2853323968ad0509313a0a1b42cb8ab21
JiHoon-0330 <ljihoon0330@gmail.com>
2025-08-21 17:54:36 +0900

    assertDate 사용하기

--- src/__tests__/hooks/easy.useCalendarView.spec.ts
+++ src/__tests__/hooks/easy.useCalendarView.spec.ts
@@ -12,7 +12,7 @@ describe('초기 상태', () => {
   it('currentDate는 오늘 날짜인 "2025-10-01"이어야 한다', () => {
     // setupTests.ts 에서 날짜 설정중
     const { result } = renderHook(() => useCalendarView());
-    expect(result.current.currentDate).toEqual(new Date('2025-10-01'));
+    assertDate(result.current.currentDate, new Date('2025-10-01'));
   });
 
   it('holidays는 10월 휴일인 개천절, 한글날, 추석이 지정되어 있어야 한다', () => {
@@ -43,7 +43,7 @@ it("주간 뷰에서 다음으로 navigate시 7일 후 '2025-10-08' 날짜로 �
   act(() => {
     result.current.navigate('next');
   });
-  expect(result.current.currentDate).toEqual(new Date('2025-10-08'));
+  assertDate(result.current.currentDate, new Date('2025-10-08'));
 });
 
 it("주간 뷰에서 이전으로 navigate시 7일 후 '2025-09-24' 날짜로 지정이 된다", () => {
@@ -54,7 +54,7 @@ it("주간 뷰에서 이전으로 navigate시 7일 후 '2025-09-24' 날짜로 �
   act(() => {
     result.current.navigate('prev');
   });
-  expect(result.current.currentDate).toEqual(new Date('2025-09-24'));
+  assertDate(result.current.currentDate, new Date('2025-09-24'));
 });
 
 it("월간 뷰에서 다음으로 navigate시 한 달 후 '2025-11-01' 날짜여야 한다", () => {
@@ -65,7 +65,7 @@ it("월간 뷰에서 다음으로 navigate시 한 달 후 '2025-11-01' 날짜여
   act(() => {
     result.current.navigate('next');
   });
-  expect(result.current.currentDate).toEqual(new Date('2025-11-01'));
+  assertDate(result.current.currentDate, new Date('2025-11-01'));
 });
 
 it("월간 뷰에서 이전으로 navigate시 한 달 전 '2025-09-01' 날짜여야 한다", () => {
@@ -76,7 +76,7 @@ it("월간 뷰에서 이전으로 navigate시 한 달 전 '2025-09-01' 날짜여
   act(() => {
     result.current.navigate('prev');
   });
-  expect(result.current.currentDate).toEqual(new Date('2025-09-01'));
+  assertDate(result.current.currentDate, new Date('2025-09-01'));
 });
 
 it("currentDate가 '2025-03-01' 변경되면 3월 휴일 '삼일절'로 업데이트되어야 한다", async () => {
